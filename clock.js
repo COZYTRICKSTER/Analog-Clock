15ce6edc7423a42be0715a32154c600a1ff429f4
COZYTRICKSTER <72147962+COZYTRICKSTER@users.noreply.github.com>
2023-12-06 11:38:13 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,14 +1,21 @@
 setInterval(() => {
-d = new Date();
-htime= d.getHours();
-mtime= d.getMinutes();
-stime= d.getSeconds();
-hrotation=30*htime + mtime/2;
-mrotation= 6*mtime;
-srotation= 6*stime;
+  fetch("https://worldtimeapi.org/api/ip")
+    .then((response) => response.json())
+    .then((data) => {
+      // 'data' now contains information about the current date and time
+      d = new Date(data.datetime);
+      console.log(d);
+    })
+    .catch((error) => console.error("Error fetching time:", error));
 
-hour.style.transform=`rotate(${hrotation}deg)`;
-minute.style.transform=`rotate(${mrotation}deg)`;
-second.style.transform=`rotate(${srotation}deg)`;
-},1000);
- 
+  htime = d.getHours();
+  mtime = d.getMinutes();
+  stime = d.getSeconds();
+  hrotation = 30 * htime + mtime / 2;
+  mrotation = 6 * mtime;
+  srotation = 6 * stime;
+
+  hour.style.transform = `rotate(${hrotation}deg)`;
+  minute.style.transform = `rotate(${mrotation}deg)`;
+  second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
